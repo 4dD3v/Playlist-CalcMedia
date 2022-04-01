b3b3692ab22699a25acb3cf31f1c3d008444c451
AdDev <89554784+4dD3v@users.noreply.github.com>
2022-04-01 20:38:50 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Luiz Gonzaga",
+    artist: "Musicas 24hrs",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Racionais Mc's",
+    artist: "Musicas 24hrs",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Turma do Pagode",
+    artist: "Musicas 24hrs",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Jose Augusto e Fernando Mendes",
+    artist: "Musicas 24hrs",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Santana o Cantador",
+    artist: "Musicas 24hrs",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Bezerra da Silva",
+    artist: "Musicas 24hrs",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
